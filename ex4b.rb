5095082d533a0909c2c9060e22d435ff2ec1cdb8
Billy Davidson <billy.davids52@gmail.com>
2018-07-17 13:19:54 -0400

    ageism

---
+++ ex4b.rb
@@ -0,0 +1,10 @@
+puts "Please enter your age"
+age = gets.to_i
+relative_age = age - 29
+
+puts "You are #{relative_age} years apart from me"
+
+if age > 105
+  puts "I'm not sure I believe you"
+
+end
